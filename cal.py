740f2eac4b09ebdc2a7c74d08c40b0285d1cd055
Daiya-Mita <daiyamaxus@gmail.com>
2018-03-11 18:02:18 +0900

    minor_update_calpy

--- cal.py
+++ cal.py
@@ -1,15 +1,30 @@
 from PIL import Image
-#import cv2
 import os
 
-def createRotateImage():
-    pict_path_list = os.listdir("./train")
+def createRotateImage(src):
+
+    if os.path.exists(src):
+        pass
+    else:
+        print(src + "フォルダが存在しないため終了します")
+        quit()
+    if os.path.exists("./rotateimage"):
+        pass
+    else:
+        print("rotateimageフォルダを作成します")
+        os.mkdir("./rotateimage")
+
+    pict_path_list = os.listdir(src)
     for pict_path in pict_path_list:
-        raw_data = Image.open("./train/" + pict_path)
+        raw_data = Image.open(src + "/" + pict_path)
         rotate_90 = raw_data.rotate(90, expand = True)
         rotate_90.save("./rotateimage/" + pict_path[:-4] +"_90.jpg")
+        rotate_180 = raw_data.rotate(180, expand=True)
+        rotate_180.save("./rotateimage/" + pict_path[:-4] + "_180.jpg")
+        rotate_270 = raw_data.rotate(270, expand=True)
+        rotate_270.save("./rotateimage/" + pict_path[:-4] + "_270.jpg")
         break
 
 
 if __name__ == "__main__":
-    createRotateImage()
+    createRotateImage("./train")
