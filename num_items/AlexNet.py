efabb7f55c0505e6eefce564ba681932f5680f26
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-04-07 14:11:54 +0900

    add num_items

---
+++ num_items/AlexNet.py
@@ -0,0 +1,145 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import cross_validation
+from keras.initializers import TruncatedNormal, Constant
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.layers import Input, Dropout, Flatten, Conv2D, MaxPooling2D, Dense, Activation, BatchNormalization
+from keras.callbacks import Callback, EarlyStopping
+from keras.utils.np_utils import to_categorical
+
+from makedata import MakeMultiDataSets, MakeSingleDataSet
+
+"""
+AlexNetを使い、画像データから店舗種別を判別する。
+"""
+
+ROOT_DIR = "./rotateimage_num_items"
+#ROOT_DIR = "./train2"
+CATEGORIES = [1,2,3,4]
+#CATEGORIES_DICT = {'ファミリマート':0, 'ファミマ!!':1, 'サンクス':2, 'サークルK':3}
+NB_CLASSES = len(CATEGORIES)
+IMAGE_SIZE_W = 128
+IMAGE_SIZE_H = 128
+
+# 拾い物
+def conv2d(filters, kernel_size, strides=1, bias_init=1, **kwargs):
+    trunc = TruncatedNormal(mean=0.0, stddev=0.01)
+    cnst = Constant(value=bias_init)
+    return Conv2D(
+        filters,
+        kernel_size,
+        strides=strides,
+        padding='same',
+        activation='relu',
+        kernel_initializer=trunc,
+        bias_initializer=cnst,
+        **kwargs
+    )
+
+def dense(units, **kwargs):
+    trunc = TruncatedNormal(mean=0.0, stddev=0.01)
+    cnst = Constant(value=1)
+    return Dense(
+        units,
+        activation='tanh',
+        kernel_initializer=trunc,
+        bias_initializer=cnst,
+        **kwargs
+    )
+
+def AlexNet():
+    model = Sequential()
+
+    # 第1畳み込み層
+    model.add(conv2d(96, 11, strides=(4,4), bias_init=0, input_shape=(IMAGE_SIZE_H, IMAGE_SIZE_W, 3)))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(BatchNormalization())
+
+    # 第２畳み込み層
+    model.add(conv2d(256, 5, bias_init=1))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(BatchNormalization())
+
+    # 第３~5畳み込み層
+    model.add(conv2d(384, 3, bias_init=0))
+    model.add(conv2d(384, 3, bias_init=1))
+    model.add(conv2d(256, 3, bias_init=1))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(BatchNormalization())
+
+    # 密結合層
+    model.add(Flatten())
+    model.add(dense(4096))
+    model.add(Dropout(0.5))
+    model.add(dense(4096))
+    model.add(Dropout(0.5))
+
+    # 読み出し層
+    model.add(Dense(NB_CLASSES, activation='softmax'))
+
+    model.compile(optimizer=SGD(lr=0.01), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+def main():
+#    md = MakeMultiDataSets(ROOT_DIR, IMAGE_SIZE_W, IMAGE_SIZE_H, 10, isSaveData=True)
+#    md = MakeSingleDataSet(ROOT_DIR, IMAGE_SIZE_W, IMAGE_SIZE_H)
+#    X, Y = md.getDataSet()
+    X = np.load('./num_items/dataX_num_items.npy')
+    Y = np.load('./num_items/dataY_num_items.npy')
+    trX, valX, trY, valY = cross_validation.train_test_split(X, Y, test_size = 0.3)
+#    for i in range(10):
+#        X = np.load("./cn_name/dataX" + str(i) + ".npy")
+#        Y = np.load("./cn_name/dataY" + str(i) + ".npy")
+#    for i, d in enumerate(datasets):
+
+    trX = np.array(trX)
+    trY = to_categorical(trY, NB_CLASSES)
+
+    valX = np.array(valX)
+    valY = to_categorical(valY, NB_CLASSES)
+
+#    X = np.array(X)
+#    Y = to_categorical(Y, NB_CLASSES)
+
+    model = AlexNet()
+
+    # モデルのコンパイル
+    model.compile(optimizer=SGD(lr=0.01),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # モデルの推定
+    history = model.fit(trX, trY,
+                        nb_epoch=50,
+                        verbose=1,
+                        validation_data=(valX, valY))
+
+    # save model
+    model.save('./num_items/CNN_num_items.h5')
+
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+#        del model #メモリからmodelを消去
+#        del X
+#        del Y
+
+if __name__ == '__main__':
+    main()
---
+++ num_items/makedata.py
@@ -0,0 +1,165 @@
+import cv2
+import pandas as pd
+import numpy as np
+import os, glob
+
+from PIL import Image
+from tqdm import tqdm
+
+class MakeDataSetBase(object):
+
+    """
+    root_dir以下のjpg画像からデータセットを生成するベースクラス
+    """
+
+    def __init__(self, root_dir, image_size_w, image_size_h):
+        self.filepaths = glob.glob(root_dir + "/*.jpg") #ファイルパス
+        self.filenames = os.listdir(root_dir) #ファイル名
+
+        self.image_size_w = image_size_w #画像をリサイズする横幅
+        self.image_size_h = image_size_h #画像をリサイズする縦幅
+
+    def imagePreprocessing(self, filepath):
+        #画像の前処理
+        img = cv2.imread(filepath)
+        img = cv2.resize(img, (self.image_size_w, self.image_size_h)) #画像をリサイズ
+        return img.astype(float) / 255
+
+    def getDataSet(self):
+        return None
+
+class MakeSingleDataSet(MakeDataSetBase):
+
+    """
+    root_dir以下のjpg画像から単一のデータセットを生成するクラス
+
+    input:
+        root_dir
+        image_size_w
+        image_size_h
+        labelpath
+    output:
+        (X, Y)
+    """
+
+    def __init__(self, root_dir, image_size_w, image_size_h):
+        MakeDataSetBase.__init__(self, root_dir, image_size_w, image_size_h)
+        self.labeldata = pd.read_csv('num_items.csv') #ラベルのデータ
+        self.categories = [1,2,3,4] # カテゴリー名
+        self.categories_dict = {1:0, 2:1, 3:2, 4:3}
+
+    def getLabel(self, filename):
+        # labeldataから当該ファイルのラベルを検索して返す
+        df = self.labeldata
+        label = df['num_items'][df.file_name == filename[:-8] + ".jpg"] # ファイル名の前半部分からラベルを検索
+        label_num = self.categories_dict[label.iloc[0]]
+        return label_num
+
+    def getDataSet(self):
+        Y=[]
+        X=[]
+        for fp, fn in tqdm(zip(self.filepaths, self.filenames)):
+            try:
+                y = self.getLabel(fn)
+                x = self.imagePreprocessing(fp)
+                Y.append(y)
+                X.append(x)
+#            except OSError:
+#                continue
+            except IndexError:
+                continue
+#            except AttributeError:
+#                continue
+#            except cv2.error:
+#                continue
+        X = np.array(X)
+        Y = np.array(Y)
+        np.save("./num_items/dataX_num_items.npy", X) #分割したデータを保存
+        np.save("./num_items/dataY_num_items.npy", Y) #分割したデータを保存
+        return (X,Y)
+
+class MakeMultiDataSets(MakeSingleDataSet):
+    """
+    root_dir以下のjpg画像を分割し、複数のデータセットを生成するクラス
+    """
+
+    def __init__(self, root_dir, image_size_w, image_size_h, nb_datasets, isSaveData=False):
+        MakeSingleDataSet.__init__(self, root_dir, image_size_w, image_size_h)
+        self.nb_datasets =nb_datasets # 分割するデータセット数
+        self.isSaveData = isSaveData #データを保存するかどうか
+
+    def getDataSet(self):
+        filepathlist = np.array_split(self.filepaths, self.nb_datasets)
+        filenamelist = np.array_split(self.filenames, self.nb_datasets)
+        res = []
+        cnt=0
+        for filepaths, filenames in zip(filepathlist, filenamelist):
+            X=[]
+            Y=[]
+#            if cnt <=8:
+#                cnt +=1
+#                continue
+            for fp, fn in tqdm(zip(filepaths, filenames)):
+                try:
+                    x = self.imagePreprocessing(fp)
+                    y = self.getLabel(fn)
+                    X.append(x)
+                    Y.append(y)
+                except OSError:
+                    continue
+                except IndexError:
+                    continue
+                except AttributeError:
+                    continue
+                except cv2.error:
+                    continue
+            X = np.array(X)
+            Y = np.array(Y)
+            if self.isSaveData:
+                np.save("./cn_name/dataX" + str(cnt) +".npy", X) #分割したデータを保存
+                np.save("./cn_name/dataY" + str(cnt) +".npy", Y) #分割したデータを保存
+            res.append((X,Y))
+            cnt+=1
+            del X
+            del Y
+        return res
+
+class MakeTestDataSet(MakeDataSetBase):
+    """
+    root_dir以下のjpg画像からテスト用のラベル無しデータセットを生成する。
+    """
+
+    def __init__(self, root_dir, image_size_w, image_size_h):
+        MakeDataSetBase.__init__(self, root_dir, image_size_w, image_size_h)
+
+    def getDataSet(self):
+        X=[]
+        pict_names = []
+        for fp, fn in tqdm(zip(self.filepaths, self.filenames)):
+            try:
+                x = self.imagePreprocessing(fp)
+                X.append(x)
+                pict_names.append(fn)
+            except OSError:
+                continue
+            except IndexError:
+                continue
+            except AttributeError:
+                continue
+            except cv2.error:
+                continue
+            except RuntimeError:
+                continue
+        return (np.array(X), np.array(pict_names))
+
+if __name__ == '__main__':
+    ROOT_DIR = "./rotateimage_num_items"
+    IMAGE_SIZE_W = 128
+    IMAGE_SIZE_H = 128
+
+    md = MakeSingleDataSet(ROOT_DIR, IMAGE_SIZE_W, IMAGE_SIZE_H)
+    datasets = md.getDataSet()
+#    md = MakeTestDataSet(ROOT_DIR, IMAGE_SIZE_W, IMAGE_SIZE_H)
+#    X = md.getDataSet()
+#    np.save("./cn_name/data_test.npy", X[0])
+#    np.save("./cn_name/test_pict_names.npy", X[1])
